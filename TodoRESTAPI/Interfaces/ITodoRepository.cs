103ba9dadf706e4e5615ca20c066e44bf2393c08
Alfredo Garcia <alfredgarcia.au@gmail.com>
2022-05-16 22:15:19 +1000

    Initial check-in of module TodoRESTAPI

---
+++ TodoRESTAPI/Interfaces/ITodoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoRESTAPI.Models;
+
+namespace TodoRESTAPI.Interfaces
+{
+    public interface ITodoRepository
+    {
+        bool DoesItemExist(string id);
+        IEnumerable<TodoItem> All { get; }
+        TodoItem Find(string id);
+        void Insert(TodoItem item);
+        void Update(TodoItem item);
+        void Delete(string id);
+    }
+}
---
+++ TodoRESTAPI/Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoRESTAPI.Models
+{
+    public class TodoItem
+    {
+        [Required]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
